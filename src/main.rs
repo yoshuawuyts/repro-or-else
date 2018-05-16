3177db6f02024bb801a30d47dff60a9673e568b8
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-05-16 18:36:01 +0200

    explain further

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,23 @@ extern crate failure;
 use failure::Error;
 
 fn main() {
-    let res: Result<usize, Error> = cache_get().ok_or_else(|| db_get());
-    println!("{:?}", res);
+    // Return type is `Ok(12)`.
+    let res = cache_get1().ok_or_else(|| db_get());
+    println!("1. {:?}", res);
+    println!("1. {:?}", res.unwrap());
+
+    // Return type is `Err(Ok(13))`.
+    let res = cache_get2().ok_or_else(|| db_get());
+    println!("2. {:?}", res);
+    println!("2. {:?}", res.unwrap());
 }
 
-fn cache_get() -> Option<usize> {
+fn cache_get1() -> Option<usize> {
     Some(12)
+}
+
+fn cache_get2() -> Option<usize> {
+    None
 }
 
 fn db_get() -> Result<usize, Error> {
