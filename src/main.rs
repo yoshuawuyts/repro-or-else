c06b2fde1d36cb16093a356beaeb9ec86cc21e85
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-05-16 18:49:39 +0200

    add type annotations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-extern crate failure;
-
-use failure::Error;
+use std::error::Error;
 
 fn main() {
     // Return type is `Ok(12)`.
-    let res = cache_get1().ok_or_else(|| db_get());
+    let res: Result<usize, Result<usize, Box<Error>>> = cache_get1().ok_or_else(|| db_get());
     println!("1. {:?}", res);
     println!("1. {:?}", res.unwrap());
 
     // Return type is `Err(Ok(13))`.
-    let res = cache_get2().ok_or_else(|| db_get());
+    let res: Result<usize, Result<usize, Box<Error>>> = cache_get2().ok_or_else(|| db_get());
     println!("2. {:?}", res);
     println!("2. {:?}", res.unwrap());
 }
@@ -22,6 +20,6 @@ fn cache_get2() -> Option<usize> {
     None
 }
 
-fn db_get() -> Result<usize, Error> {
+fn db_get() -> Result<usize, Box<Error>> {
     Ok(13)
 }
